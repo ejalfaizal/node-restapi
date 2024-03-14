a04f2201ea9585ee681b935c642579aaaa9a8f2a
Hanafiah Hassan <99636296+hanafiah-enovade@users.noreply.github.com>
2024-03-15 00:25:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 
-// TODO: Exercise 2
+app.use(express.json());
+
+/**
+ * GET request route that returns "Salam Malaysia Madani!!" as JSON data.
+ * @name GET/api/
+ * @function
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
+app.get('/api/', (req, res) => {
+    res.json({ message: 'Salam Malaysia Madani' });
+});
 
 
 // TODO: Exercise 5
